b1789ebe9a7df15af55d3696f285891047fb23bf
JonathanFeld11 <820438@mystma.org>
2022-09-16 08:48:09 -0500

    2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #WRITE YOUR CODE IN THIS FILE
 def topFiveMovies(a, b, c, d, e):
-    return a + b + c + d + e
+    return a + " " + b  + " " + c + " " + d + " " + e
 print (topFiveMovies("Kung Fu Panda ", "Kung Fu Panda 2 ", "Kung Fu Panda 3 ", "Kung Fu Panda Secrets of the Scroll ", "Shrek "))
